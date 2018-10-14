9682714665698d6353687a00ccca4fd88fa0c837
grimmdaniel <grimmdani@inf.elte.hu>
2018-10-14 17:00:37 +0200

    small ui fixes

--- ChessKitServer/src/main/java/chesskit/chesskitserver/Main.java
+++ ChessKitServer/src/main/java/chesskit/chesskitserver/Main.java
@@ -11,10 +11,11 @@ package chesskit.chesskitserver;
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
