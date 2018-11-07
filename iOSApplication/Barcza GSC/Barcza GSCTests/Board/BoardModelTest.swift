c977ea7695924c8a3b4b8790865c26c8745c7033
grimmdaniel <grimmdani@inf.elte.hu>
2018-11-07 19:11:39 +0100

    structured tests

---
+++ iOSApplication/Barcza GSC/Barcza GSCTests/Board/BoardModelTest.swift
@@ -0,0 +1,26 @@
+//
+//  BoardModelTest.swift
+//  Barcza GSCTests
+//
+//  Created by Grimm Dániel on 2018. 11. 07..
+//  Copyright © 2018. daniel.grimm. All rights reserved.
+//
+
+import XCTest
+@testable import ChessBoardKit
+
+class BoardModelTest: XCTestCase {
+    
+    var boardModel: BoardModel!
+    
+    override func setUp() {
+        boardModel = BoardModel()
+        
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+}
