b16aa709efe7b9864a09a532447ca06fe634e220
grimmdaniel <grimmdani@inf.elte.hu>
2018-11-26 22:09:41 +0100

    boardmodel tested

--- iOSApplication/Barcza GSC/Barcza GSCTests/Board/BoardModelTest.swift
+++ iOSApplication/Barcza GSC/Barcza GSCTests/Board/BoardModelTest.swift
@@ -15,12 +15,42 @@ class BoardModelTest: XCTestCase {
     
     override func setUp() {
         boardModel = BoardModel()
-        
+        boardModel.initializeBoard()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testEmulateThreat(){
+        XCTAssertEqual(boardModel.emulateThreatSearch(with: boardModel.findKingPosition(withSide: .black)!).count, 0)
     }
-
     
+    func testPawnStatus(){
+        XCTAssertEqual(boardModel.getPawnStatus(), PawnReachedOppositeSideEnd.none)
+    }
+    
+    func testFindWhiteKingPosition(){
+        XCTAssertEqual(boardModel.findKingPosition(withSide: .white)?.position, Coords(rank: 7, file: 4))
+    }
+    
+    func testFindBlackKingPosition(){
+        XCTAssertEqual(boardModel.findKingPosition(withSide: .black)?.position, Coords(rank: 0, file: 4))
+    }
+    
+    func testEnPassantSquare(){
+        XCTAssertEqual(boardModel.accessToEnPassantSquare(), nil)
+    }
+    
+    func testFindPromotedPawn(){
+        XCTAssert(boardModel.findPromotedPawn() == nil)
+    }
+
+    func testFENGeneration(){
+        XCTAssertEqual(boardModel.generateFENFromBoard(),"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    }
+    
+    func testThreatsForWhiteKing(){
+        XCTAssertEqual(boardModel.threatsForWhiteKing, [])
+    }
+    
+    func testThreatsForBlackKing(){
+        XCTAssertEqual(boardModel.threatsForBlackKing, [])
+    }
 }
